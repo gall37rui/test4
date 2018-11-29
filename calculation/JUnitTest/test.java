207a3933082362a34f016b6019ae8ad9708fec9e
Bayanov95liz <45388568+Bayanov95liz@users.noreply.github.com>
2018-11-29 10:30:41 +0500

    Update test.java

--- calculation/JUnitTest/test.java
+++ calculation/JUnitTest/test.java
@@ -8,10 +8,11 @@ class test {
 
 	@Test
 	void test() {
-		calc1 calcu_mounth1 = new calc1( 1000,  1, 5);
-			calc1 calcu_mounth2 = new calc1( 5000,  0.2, 5);
-			calc1 calcu_mounth3 = new calc1( 10,  0.01, 1);
-			calc1 calcu_mounth4 = new calc1( 10101,  0.10101, 101);
+		
+		calculation.calc1 calcu_mounth1 = new calculation.calc1( 1000,  1, 5);
+		calculation.calc1 calcu_mounth2 = new calculation.calc1( 5000,  0.2, 5);
+		calculation.calc1 calcu_mounth3 = new calculation.calc1( 10,  0.01, 1);
+		calculation.calc1 calcu_mounth4 = new calculation.calc1( 10101,  0.10101, 101);
 			assertSame(calcu_mounth1,calcu_mounth4);
 			assertSame(calcu_mounth2,calcu_mounth3);
 			
