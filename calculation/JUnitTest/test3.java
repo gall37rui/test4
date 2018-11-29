4bd506671a2878913822324934036b46226d9721
student <student@Gamma2>
2018-11-29 11:27:28 +0300

    Test

--- calculation/JUnitTest/test3.java
+++ calculation/JUnitTest/test3.java
@@ -1,22 +1,19 @@
 package JUnitTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class test3 {
+public class test3 {
 
 	@Test
-	void test() {
-		
+	public void test() {
 		calculation.calc1 calcu_mounth1 = new calculation.calc1( 1000,  1, 5);
 		calculation.calc1 calcu_mounth2 = new calculation.calc1( 5000,  0.2, 5);
 		calculation.calc1 calcu_mounth3 = new calculation.calc1( 10,  0.01, 1);
 		calculation.calc1 calcu_mounth4 = new calculation.calc1( 10101,  0.10101, 101);
 			assertSame(calcu_mounth1,calcu_mounth4);
 			assertSame(calcu_mounth2,calcu_mounth3);
-			
-
 	}
 
 }
